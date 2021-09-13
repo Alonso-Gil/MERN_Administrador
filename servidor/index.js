e6993bbaa28f2e550f1740dae378d06c69f7ac45
Alonso-Gil <alonso-gil117@gmail.com>
2021-09-13 11:00:56 -0500

    JWT creado - borrar el .env

---
+++ servidor/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const conectarDB = require('./config/db');
+
+// Crear el servidor
+const app = express();
+
+// Conectar a la base de datos
+conectarDB();
+
+// Habilitar express.json
+app.use(express.json({ extended: true }));
+
+// Puerto de la app
+const PORT = process.env.PORT || 4000;
+
+// Importar rutas
+app.use('/api/usuarios', require('./routes/usuarios'));
+
+// Arranca la app
+app.listen(PORT, () => {
+    console.log(`El servidor esta funcionando en el puerto ${PORT}`);
+})
+
